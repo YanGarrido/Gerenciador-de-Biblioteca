1fea35769dcbc480cf3d0bd6d3c41d3a26f294dc
Yan <yangarridodev@gmail.com>
2025-09-19 18:31:31 -0300

    feat: criação da classe Autor

---
+++ src/Gerenciamento/Autor.java
@@ -0,0 +1,49 @@
+package Gerenciamento;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+public class Autor {
+  private int id;
+  private String nome;
+  private Date dataNascimento;
+
+  public Autor(int id, String nome, Date dataNascimento) {
+    this.id = id;
+    this.nome = nome;
+    this.dataNascimento = dataNascimento;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public String getNome() {
+    return nome;
+  }
+
+  public Date getDataNascimento() {
+    return dataNascimento;
+  }
+
+  public void setDataNascimento(Date dataNascimento) {
+    this.dataNascimento = dataNascimento;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  @Override
+  public String toString() {
+    return "Autor{" +
+        "id=" + id +
+        ", nome='" + nome + '\'' +
+        ", dataNascimento=" + dataNascimento +
+        '}';
+  }
+}
